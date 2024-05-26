d072a7f05a955c2b9eabe8ccb493e6be168706a1
awaisAhmed19 <awaisahmedoffi@gmail.com>
2024-05-27 00:03:13 +0530

    added drawing logic and made some changes in the tools css

--- Frontend/src/static/js/Tools.js
+++ Frontend/src/static/js/Tools.js
@@ -5,6 +5,7 @@ const selectedColor = document.querySelector('.selectedcolor');
 
 let currentColor = selectedColor.style.backgroundColor;
 let cursorType;
+let isDrawing = false;
 
 //have to find a way to put thr tool icon images back in the css background-image rule
  
@@ -15,22 +16,50 @@ tools.forEach((tool) => {
             t.classList.remove('pressed');
         });
         e.target.classList.add('pressed');
-        console.log('Clicked element:', e.target);
-        console.log('Clicked element ID:', e.target.id);
     });
 });
 
-
-class Tools{
-    constructor(name,cursorType,size,currentColor) {
-        this.name = name;
-        this.cursorType = cursorType;
-        this.size = size;
-        this.selectedColor = selectedColor;
-        this.isActive = false;
-        this.undoStack = [];
-        this.redoStack = [];
-    }
+canvas.addEventListener('mousedown', startDrawing);
+canvas.addEventListener('mousemove', draw);
+canvas.addEventListener('mouseup', stopDrawing);
+//canvas.addEventListener('mouseout', stopDrawing);
 
 
+function getMousePos(canvas, e) {
+    const rect = canvas.getBoundingClientRect();
+    return {
+        x: e.clientX - rect.left,
+        y: e.clientY - rect.top
+    };
+}
+
+function draw(e) {
+    if (!isDrawing) return;
+    const pos = getMousePos(canvas, e);
+    ctx.lineTo(pos.x,pos.y);
+    ctx.strokeStyle = currentColor;
+    ctx.lineWidth = 1;
+    ctx.lineCap = 'round';
+    ctx.lineJoin = 'round';
+    ctx.stroke();
+}
+
+function startDrawing(e) {
+    isDrawing = true;
+    ctx.beginPath();
+    ctx.moveTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
+}
+
+
+function stopDrawing(e) {
+    isDrawing = false;
+    ctx.closePath();
+}
+
+function debounce(func, delay) {
+    let timeout;
+    return function (...args) {
+        clearTimeout(timeout);
+        timeout = setTimeout(() => func.apply(this, args), delay);
+    };
 }
