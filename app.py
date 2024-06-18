d6e7bf0bca92695ba8062abcc4fac880015958f8
awaisAhmed19 <awaisahmedoffi@gmail.com>
2024-06-19 00:41:19 +0530

    added templates for tool options

--- app.py
+++ app.py
@@ -9,5 +9,33 @@ def index():
     return render_template("Index.html")
 
 
+@app.route('/RectOptions.html')
+def template():
+    return render_template('RectOptions.html')
+
+@app.route('/ElipseTool.html')
+def template1():
+    return render_template('ElipseTool.html')
+
+@app.route('/RoundedRectTool.html')
+def template2():
+    return render_template('RoundedRectTool.html')
+
+@app.route('/PolygonOptions.html')
+def template3():
+    return render_template('PolygonOptions.html')
+
+@app.route('/LineOptions.html')
+def template4():
+    return render_template('LineOptions.html')
+
+@app.route('/CurvedLineOptions.html')
+def template5():
+    return render_template('CurvedLineOptions.html')
+
+@app.route('/AirBrushOptions.html')
+def template6():
+    return render_template('AirBrushOptions.html')
+
 if __name__ == '__main__':
     app.run(debug=True,use_reloader=True)
